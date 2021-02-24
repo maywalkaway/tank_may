d43ac2338c71cfddd44bbbad29499611ac1e228a
mayu <2584904548@qq.com>
2021-02-24 22:28:12 +0800

    Initial commit

---
+++ src/com/mashibing/tank/T.java
@@ -0,0 +1,12 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) {
+        TankFrame tf =new TankFrame();
+
+    }
+}
